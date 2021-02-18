c3d3260bb60984b56deba712918b76f95586dcaa
Julia Stabrawa <stabrawa.julka@gmail.com>
2021-02-18 13:10:04 +0100

    editing of local storage for all pages

--- development/js/app.js
+++ development/js/app.js
@@ -1,3 +1,5 @@
+
+
 //2.3 Dodawanie imienia
 
 const givenName = document.querySelector("#name");
@@ -6,34 +8,51 @@ const userName = document.querySelector(".user__name");
 const firstPage = document.querySelector(".first__entry");
 const mainDesk = document.querySelector(".desk__main");
 
+firstPage.classList.add("hide");
+
 function saveNameToLocalStorage() {
     const user = givenName.value;
     localStorage.setItem("name", user);
     userName.innerHTML = localStorage.getItem("name");
-    firstPage.classList.add("hide");
-    mainDesk.classList.add("show");
 }
-
 btnName.addEventListener("click", e => {
     e.preventDefault()
     if (givenName.value === "") {
         alert("Musisz wpisać jakieś imię");
     } else {
         saveNameToLocalStorage();
+        firstPage.classList.add("hide");
+        mainDesk.classList.remove("hide");
     }
     console.log(localStorage.getItem("name"));
 });
 
-
 window.onload = () => {
-    if (localStorage.getItem("name") !== "null") {
+    if (localStorage.getItem("name") !== null) {
         userName.innerHTML = localStorage.getItem("name");
         firstPage.classList.add("hide");
-        mainDesk.classList.add("show");
-    } else {
-        saveNameToLocalStorage();
+    }else{
+        firstPage.classList.remove("hide");
     }
 }
+
+
+//2.1 Starting page
+
+// const sidebar = document.querySelector(".sidebar");
+// const menuEl = sidebar.querySelectorAll("a");
+// const menuArrow = menuEl.querySelectorAll(".fas");
+//
+// for(let i = 0; i < menuEl.length; i++) {
+//     menuEl[i].addEventListener("click", function (e) {
+//         menuEl.forEach(el => {
+//             el.classList.remove("focus");
+//             el.parentElement.children[1].classList.add("hide");
+//         });
+//         this.classList.add("focus");
+//         this.parentElement.children[1].classList.add("show");
+//     });
+// }
 
 
 //przejscia w app
